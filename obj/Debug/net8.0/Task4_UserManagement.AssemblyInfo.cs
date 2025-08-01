ee0bd541e7f32cacd1a89311a52d921c13608ee5
Jose Salazar <jmsalazar.macg@hotmail.com>
2025-08-01 01:48:10 -0600

    runtime.json changed to global.json

--- obj/Debug/net8.0/Task4_UserManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/Task4_UserManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4_UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+811e25421acf947bc557ad1007983efecd36cb18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe68fbdace88d524fab00f05ac8ce2930a73709")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4_UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4_UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
